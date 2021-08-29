7ca890c6791fd83284448978ebd4078fff5379d1
rbaliwal00 <rajan18baliwal@gmail.com>
2021-08-29 19:02:16 +0530

    Basic Student POJO and Controller

---
+++ src/main/java/com/example/springsecurity/student/Student.java
@@ -0,0 +1,20 @@
+package com.example.springsecurity.student;
+
+public class Student {
+
+    private final Integer studentId;
+    private  final String studentName;
+
+    public Student(Integer studentId, String studentName) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+    }
+
+    public Integer getStudentId() {
+        return studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+}
